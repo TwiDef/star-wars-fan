e3f6af3d057cadf20acdc835ff76bf0c9cffa8c0
Def <deferenseo@gmail.com>
2024-07-08 14:46:29 +0300

    added list of char-info

--- src/components/Character/Character.jsx
+++ src/components/Character/Character.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router';
+import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSingleChar } from '@redux/slices/charactersSlice';
 import { setApiStatus } from '../../redux/slices/apiSlice';
@@ -8,6 +9,7 @@ import { BASE_URL, BASE_IMG_URL } from '@utils/constants';
 import { getNumFromStr } from '@utils/network';
 
 import Error from '../Error/Error';
+import Loader from '../Loader/Loader';
 
 import styles from './Character.module.css';
 
@@ -17,6 +19,7 @@ const Character = () => {
   const { apiError } = useSelector(state => state.api)
   const params = useParams()
   const id = params.id
+  const navigate = useNavigate()
 
   const getCharacter = async (url) => {
     try {
@@ -45,13 +48,46 @@ const Character = () => {
     <>
       {apiError ? <Error /> :
         singleCharacter ?
-          <div>
-            <p>{singleCharacter.name}</p>
-            <img
-              src={`${BASE_IMG_URL}/characters/${getNumFromStr(singleCharacter.url)}.jpg`}
-              alt="char-img" />
-          </div>
-          : <p>Loading...</p>}
+          <>
+            <div className={styles.wrapper}>
+              <div className={styles.image}>
+                <h2 className={styles.title}>{singleCharacter.name}</h2>
+                <button>
+                  <img className={styles.like}
+                    src="https://cdn-icons-png.flaticon.com/512/833/833472.png " alt="like" />
+                </button>
+                <img
+                  src={`${BASE_IMG_URL}/characters/${getNumFromStr(singleCharacter.url)}.jpg`}
+                  alt="char-img" />
+              </div>
+              <div className={styles.info}>
+                <h2 className={styles.infoTitle}>Character Info</h2>
+                <ul className={styles.infoList}>
+                  <li><h4> Height :</h4> {singleCharacter.height} cm</li>
+                  <li><h4> Mass :</h4> {singleCharacter.mass} kg</li>
+                  <li><h4> Hair color :</h4> {singleCharacter.hair_color}</li>
+                  <li><h4> Skin color :</h4> {singleCharacter.skin_color}</li>
+                  <li><h4> Eye color :</h4> {singleCharacter.eye_color}</li>
+                  <li><h4> Birth year :</h4> {singleCharacter.birth_year}</li>
+                  <li><h4> Gender :</h4> {singleCharacter.gender}</li>
+                </ul>
+              </div>
+              <div className={styles.filmInfo}>
+                <h2 className={styles.filmTitle}>Episodes</h2>
+                <ul>
+                  <li>Episode: X</li>
+                  <li>Episode: X</li>
+                  <li>Episode: X</li>
+                  <li>Episode: X</li>
+                </ul>
+              </div>
+            </div>
+
+            <button className={styles.goBackBtn} onClick={() => navigate(-1)}>
+              go back
+            </button>
+          </>
+          : <Loader />}
     </>
   );
 };
